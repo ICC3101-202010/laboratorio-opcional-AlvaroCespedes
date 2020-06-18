ac28b45e4b75909a47d818fa30c2bcebee74da38
AlvaroCespedes <aecespedes@miuandes.cl>
2020-06-18 16:50:23 -0400

    first commit

---
+++ Opcional proyecto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Opcional_proyecto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
